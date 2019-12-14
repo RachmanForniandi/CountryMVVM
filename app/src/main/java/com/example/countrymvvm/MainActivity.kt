db5138d9d68ac92d001b62cc051c2cbad27f6bce
RachmanForniandi <rachmanforniandi@gmail.com>
2019-12-14 16:22:13 +0700

    set adapter on class activity

--- app/src/main/java/com/example/countrymvvm/MainActivity.kt
+++ app/src/main/java/com/example/countrymvvm/MainActivity.kt
@@ -2,11 +2,50 @@ package com.example.countrymvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+import com.example.countrymvvm.adapter.CountryAdapter
+import com.example.countrymvvm.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel:ListViewModel
+    private val countryAdapter =CountryAdapter(arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        listCountry.apply {
+            adapter = countryAdapter
+        }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.countries.observe(this, Observer {
+            countries  -> countries?.let { countryAdapter.updateCountries(it) }
+        })
+
+        viewModel.countryLoadWhenError.observe(this,Observer{
+            isError -> isError?.let { tvWhenError.visibility = if(it) View.VISIBLE else View.GONE  }
+        })
+
+        viewModel.countryLoadWhenError.observe(this,Observer{
+                isLoading -> isLoading?.let { progressView.visibility = if(it) View.VISIBLE else View.GONE
+
+                if (it){
+                    tvWhenError.visibility = View.GONE
+                    listCountry.visibility = View.GONE
+                }
+            }
+        })
     }
 }
